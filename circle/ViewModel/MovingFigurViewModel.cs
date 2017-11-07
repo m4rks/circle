d53cc49509bcc3fe22ffd9d6cfd7693beaaebbee
m4rks <michalak.marek@gmail.com>
2017-11-07 10:11:22 +0100

    1 zmiana

--- circle/ViewModel/MovingFigurViewModel.cs
+++ circle/ViewModel/MovingFigurViewModel.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace circle.ViewModel
+﻿namespace circle.ViewModel
 {
     class MovingFigurViewModel
     {
+        private int Aaa;
     }
 }
